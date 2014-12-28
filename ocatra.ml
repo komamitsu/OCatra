b66d9d4df4c6438b20395d113911208cdf901a17
Mitsunori Komatsu <komamitsu@gmail.com>
2014-12-29 01:11:10 +0900

    Improve OCatra.Routes to handle recursive path matching

--- ocatra.ml
+++ ocatra.ml
@@ -7,35 +7,70 @@ module Proc = struct
   type t = OcatraHttpRequest.t -> OcatraHttpResponse.t
 end
 
+(* TODO: move this module to a new file *)
 module Routes = struct
-  type t = (string, Proc.t) Hashtbl.t
-  let init () : t = Hashtbl.create 10
-  let bind = Hashtbl.replace
-  let find = Hashtbl.find
+  type 'a t = (string * Str.regexp * 'a) list
+
+  let init () = []
+
+  let remove_tail_slath path =
+    if path = "/" then path
+    else begin
+      let len = String.length path in
+      if path.[len - 1] = '/' then
+        String.sub path 0 (len - 1)
+      else
+        path
+    end
+
+  let bind routes path proc =
+    let path = remove_tail_slath path in
+    let re = Str.regexp ("^" ^ path ^ "\\b") in
+    let new_route = (path, re, proc)::routes in
+    List.sort (fun (path0, _, _) (path1, _, _) -> compare path1 path0) new_route
+
+  let find routes path = 
+    if path = "/" then
+      let (_, _, proc) =
+        List.find (fun (p, _, _) -> p = path) routes in
+      proc
+    else
+      let path = remove_tail_slath path in
+      let (_, _, proc) =
+        List.find (fun (_, re, _) -> 
+          Str.string_match re path 0) routes in
+      proc
 end
 
-let get_routes = Routes.init ()
-let post_routes = Routes.init ()
-let put_routes = Routes.init ()
-let delete_routes = Routes.init ()
+let get_routes = ref (Routes.init ())
+let post_routes = ref (Routes.init ())
+let put_routes = ref (Routes.init ())
+let delete_routes = ref (Routes.init ())
 
-let get path f = Routes.bind get_routes path f
-let post path f = Routes.bind post_routes path f
-let put path f = Routes.bind put_routes path f
-let delete path f = Routes.bind delete_routes path f
+let get path f =
+  get_routes := Routes.bind !get_routes path f
+
+let post path f =
+  post_routes := Routes.bind !post_routes path f
+
+let put path f =
+  put_routes := Routes.bind !put_routes path f
+
+let delete path f =
+  delete_routes := Routes.bind !delete_routes path f
 
 let run ?conf:(conf=OcatraConfig.create ()) () =
   OcatraHttpServer.start conf
     (fun req ->
-      let route =
+      let routes =
         match req.methd with
-        | Method.Get -> get_routes
-        | Method.Post -> post_routes
-        | Method.Put -> put_routes
-        | Method.Delete -> delete_routes
+        | Method.Get -> !get_routes
+        | Method.Post -> !post_routes
+        | Method.Put -> !put_routes
+        | Method.Delete -> !delete_routes
       in
       try
-        let proc = Routes.find route req.path in proc req
+        let proc = Routes.find routes req.path in proc req
       with 
       | HttpError st -> create_response ~status:st
           (Content.TextPlain (Status.string_of_status st)) ()
